28249df75f9a5b8f0acfe9f1ed7eda2574681b09
miodunek <marcelimiodunek@gmail.com>
2023-09-02 00:25:24 +0200

    Update reading from and saving to file

--- main.py
+++ main.py
@@ -8,34 +8,48 @@ def display_menu():
     print("1. Add a task\t 2. Show tasks\t 3. Mark as completed\t 4. Quit")
 
 
+def save_to_file(task_index):
+    path = "C:/Users/marce/Desktop/codes/to-do-list"
+
+    if os.path.exists(path):
+        file = open("to-do-list.txt", "a")
+
+        file.write(f"{task_index}.")
+        for values in task_list[task_index].values():
+            file.write(values + " ")
+        file.write("\n")
+        file.close()
+    else:
+        print("File not found")
+
+
 def add_task():
     task = input("Add a task: ")
-    time = datetime.now()
+    time = str(datetime.now())
     index = int(len(task_list) + 1)
     task_list[index] = {"task": task, "time": time, "completed": "Not completed"}
+    save_to_file(index)
     print("Task added!")
 
 
 def display_tasks():
-    for i in range(1, len(task_list) + 1):
-        print(f"{i}.")
-        for values in task_list[i].values():
-            print(values)
+    file = open("to-do-list.txt", "r")
+    data = file.read()
+    print(data)
+    file.close()
 
 
 def mark_done():
     done_task = int(input("Which task do you want to mark as completed?"))
     task_list[done_task]['completed'] = "Completed"
-
-
-def save_to_file():
-    path = "C:/Users/marce/Desktop/codes/to-do-list"
-
-    if os.path.exists(path):
-        file = open("to-do-list.txt", "a")
-        for i in range(1, len(task_list) + 1):
-            file.write(f"{i}. {task_list[i].values()}")
-        file.close()
+    file = open("to-do-list.txt", "r")
+    data = file.read()
+    data = data.replace(task_list[done_task]['completed'], 'Completed')
+    file.close()
+    file = open("to-do-list.txt", "w")
+    file.write(data)
+    file.close()
+    print("Marked as completed!")
 
 
 while True:
@@ -49,5 +63,4 @@ while True:
     elif user_input == 3:
         mark_done()
     elif user_input == 4:
-        save_to_file()
         break
